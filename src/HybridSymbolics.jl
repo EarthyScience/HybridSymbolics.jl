6eb7038a426bdc6004fc35e2528c4e567d5cdeb9
Lazaro Alonso <lazarus.alon@gmail.com>
2024-11-21 10:23:51 +0100

    more pkgs

--- src/HybridSymbolics.jl
+++ src/HybridSymbolics.jl
@@ -1,6 +1,11 @@
 module HybridSymbolics
 
     using Symbolics
+    using Lux
+    using Optimisers
+    using NNlib
+    using ADTypes
+    using ForwardDiff
+    using ProgressMeter
     # Write your package code here.
-
 end
