0ac82619dcc7d323860175a17a8f97c0484b2963
qfl3x <qfl3x@protonmail.com>
2024-11-22 11:22:51 +0100

    Forward Inference works!
    So I essentially take the function and "optimize" it by
    rewriting it to a standard format which takes in the
    varying parameters as a tuple of Float32's and
    the globals as an array of Float32's. The fixed parameters
    are parsed away and replaced in the function body
    by their values.

--- scripts/sample.jl
+++ scripts/sample.jl
@@ -1,10 +1,10 @@
-using HybridSymbolics, Symbolics, Lux
+using HybridSymbolics, Symbolics, Lux, Random
 
 const input_size = 5
 local α, β, γ, δ
 @syms α::Real β::Real γ::Real δ::Real
 
-structured = @hybrid function testfunc(α, β, γ, δ)
+structured = @hybrid function testfunc(α::Varying, β::Varying, γ::Fixed=1.0, δ::Global)
     return (exp(α) - β)/(γ * δ)
 end
 
@@ -13,8 +13,13 @@ NN = Chain(
    Dense(4 => 3, sigmoid_fast)
 )
 
-HybridModel(testfunc(α, β, γ, δ),
-            global_vars = [δ],
-            input_size = 5,
-            nn = NN
+NN = f32(NN)
+rng = MersenneTwister()
+ps, st = Lux.setup(rng, NN)
+
+model = HybridModel(
+    NN,
+    structured
 )
+
+model(rand(5), (ps, [1.2f0]), st)
--- src/HybridSymbolics.jl
+++ src/HybridSymbolics.jl
@@ -8,6 +8,7 @@ using NNlib
 using ADTypes
 using ForwardDiff
 using ProgressMeter
+using ProtoStructs
 
 
 struct Varying
@@ -17,15 +18,17 @@ end
 struct Fixed
 end
 
-struct StructuredFunction{T1}
+@proto @kwdef struct StructuredFunction{T1, T2}
     func::T1
     args::Vector{SymbolicUtils.BasicSymbolic}
     global_args::Vector{SymbolicUtils.BasicSymbolic}
     fixed_args::Vector{SymbolicUtils.BasicSymbolic}
     varying_args::Vector{SymbolicUtils.BasicSymbolic}
+    fixed_vals::Vector{Float32}
+    opt_func::T2
 end
 
-function StructuredFunction(func, args, global_args, fixed_args, varying_args)
+function StructuredFunction(func, args, global_args, fixed_args, varying_args, fixed_vals, opt_func)
     type_wanted = SymbolicUtils.BasicSymbolic
     StructuredFunction(
         func,
@@ -33,47 +36,109 @@ function StructuredFunction(func, args, global_args, fixed_args, varying_args)
         convert(Vector{type_wanted}, global_args),
         convert(Vector{type_wanted}, fixed_args),
         convert(Vector{type_wanted}, varying_args),
+        convert(Vector{Float32}, fixed_vals),
+        opt_func
     )
 end
 
-struct HybridModel{T1, T2, T3}
+@proto @kwdef struct HybridModel{T1, T2}
     nn::Lux.Chain{T1}
     func::StructuredFunction{T2}
+end
+
+function (m::HybridModel)(X, params, st)
+    ps = params[1]
+    globals = params[2]
+    out_NN = m.nn(X, ps, st)[1]
+    return (out_NN[1], out_NN[2])
+    out = m.func.opt_func((out_NN[1], out_NN[2]), globals)
+    return out
 end
 
 function name_and_type(expr::Expr)
     @assert expr.head == :(::)
     return expr.args[1], expr.args[2]
 end
-macro hybrid(fun)
-    fun_header = fun.args[1]
-    fun_args = fun_header.args[2:end]
+
+function parse_args(fun_args, fun)
     global_args = []
     fixed_args = []
     varying_args = []
+    # Perhaps a NamedTuple or a Dict is better
+    fixed_vals = []
     all_args = []
     for (i,arg) in enumerate(fun_args)
-        name, type = name_and_type(arg)
-        if type == :Global
-            push!(global_args, name)
-        elseif type == :Fixed
+        if arg.head == :(::)
+            name, type = name_and_type(arg)
+            if type == :Global
+                push!(global_args, name)
+            elseif type == :Varying
+                push!(varying_args, name)
+            end
+            push!(all_args, name)
+            fun.args[1].args[1+i] = name
+        elseif arg.head == :kw
+            name = arg.args[1].args[1]
+            val = arg.args[2]
             push!(fixed_args, name)
-        elseif type == :Varying
-            push!(varying_args, name)
+            push!(fixed_vals, val)
+            fun.args[1].args[1+i] = name
         end
-        push!(all_args, name)
-        fun.args[1].args[1+i] = name
     end
+    return global_args, fixed_args, varying_args, fixed_vals, all_args
+end
+
+function substitute_vars(expr, subs)
+    if expr isa Symbol && haskey(subs, expr)
+        return subs[expr]
+    elseif expr isa Expr
+        new_args = [substitute_vars(arg, subs) for arg in expr.args]
+        return Expr(expr.head, new_args...)
+    else
+        return expr
+    end
+end
+
+function optimize_func(fun, global_args, fixed_args, varying_args, fixed_vals)
+    func_expr = copy(fun)
+    func_body = func_expr.args[2]
+    func_args = Expr(:tuple)
+    varying_tuple = Expr(:(::), :varying_params, Expr(:curly, :Tuple, [:Float32 for _ in varying_args]...))
+    push!(func_args.args, varying_tuple)
+    global_array = Expr(:(::), :global_params, :(Vector{Float32}))
+    push!(func_args.args, global_array)
+    substitutions = Dict()
+    for (arg, val) in zip(fixed_args, fixed_vals)
+        substitutions[arg] = val
+    end
+    for (i, arg) in enumerate(varying_args)
+        substitutions[arg] = :(varying_params[$i])
+    end
+    for (i, arg) in enumerate(global_args)
+        substitutions[arg] = :(global_params[$i])
+    end
+    new_body = substitute_vars(func_body, substitutions)   
+    new_func = Expr(:->,
+        func_args,
+        new_body
+    )
+    return new_func
+end
+macro hybrid(fun)
+    fun_header = fun.args[1]
+    fun_args = fun_header.args[2:end]
+    global_args, fixed_args, varying_args, fixed_vals, all_args = parse_args(fun_args, fun)
+    opt_func = optimize_func(fun, global_args, fixed_args, varying_args, fixed_vals)
     global_args = Expr(:vect, global_args...)
     fixed_args = Expr(:vect, fixed_args...)
     varying_args = Expr(:vect, varying_args...)
     args_syms = Expr(:vect, all_args...)
     return quote
-        StructuredFunction($(esc(fun)), $(esc(args_syms)), $(esc(global_args)), $(esc(fixed_args)), $(esc(varying_args)))
+        StructuredFunction($(esc(fun)), $(esc(args_syms)), $(esc(global_args)), $(esc(fixed_args)), $(esc(varying_args)), $(fixed_vals), $(esc(opt_func)))
     end
 end
 
-export Global, Varying, Fixed, @hybrid, StructuredFunction
+export Global, Varying, Fixed, @hybrid, StructuredFunction, HybridModel
 
 # Write your package code here.
 end
